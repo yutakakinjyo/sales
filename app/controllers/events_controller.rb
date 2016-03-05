8180d2d76845c1f05afd01739434234e2e69cd1a
yutakakinjyo <yutakakinjyo@gmail.com>
2016-03-05 17:01:58 +0900

    add form to select customer to event model

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:name, :date, :note)
+      params.require(:event).permit(:name, :date, :note, :customer_id)
     end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < ActiveRecord::Base
   has_many :states
+  has_many :events
 end
